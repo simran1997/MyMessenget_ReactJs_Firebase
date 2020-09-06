333e1dd1f6ae281cff64f890908e772553259357
simran1997 <simranjeet6344@gmail.com>
2020-09-06 00:15:36 -0400

    Update Firrebase.js

--- src/Firrebase.js
+++ src/Firrebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2PDT4X4QVhgGrDtWx3cPYC9RdeA2sfWQ",
-    authDomain: "my-messenger-cb5e2.firebaseapp.com",
-    databaseURL: "https://my-messenger-cb5e2.firebaseio.com",
-    projectId: "my-messenger-cb5e2",
-    storageBucket: "my-messenger-cb5e2.appspot.com",
-    messagingSenderId: "221278051599",
-    appId: "1:221278051599:web:5f5da88d41eeb10c60327a",
-    measurementId: "G-2CZGQGR1DJ"
+    
   };
 
   // Initialize Firebase with a "default" Firebase project
